0bac36ebe23532c00b824fce2b089f3463885c93
Manodor-S <manuel.n.stoeth@outlook.de>
2025-02-06 09:58:11 +0100

    removed spring?

--- src/main/java/de/thws/fiw/gymmanagement/infrastructure/BookingRepositoryInterface.java
+++ src/main/java/de/thws/fiw/gymmanagement/infrastructure/BookingRepositoryInterface.java
@@ -1,12 +1,11 @@
 package de.thws.fiw.gymmanagement.infrastructure;
 
 import de.thws.fiw.gymmanagement.domain.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BookingRepositoryInterface extends JpaRepository<Booking, Long> {
+public interface BookingRepositoryInterface {
     Booking save(Booking booking);
     Optional<Booking> findById(Long id);
     List<Booking> findByMemberId(Long memberId);
